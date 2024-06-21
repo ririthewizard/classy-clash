207dd72ac8b6e04e0392d03a02da638a5efa419a
Riley <ririwilliams.rw@gmail.com>
2024-06-20 21:53:47 -0500

    Drew the knight. Used DrawTexturePro which gives us an easier time to draw in the middle of the screen. Scaled up the knight by 6 to match the worldMap scale

--- main.cpp
+++ main.cpp
@@ -11,6 +11,13 @@ int main()
 
     float speed{6.0};
 
+    Texture2D knight = LoadTexture("characters/knight_idle_spritesheet.png");
+    Vector2 knightPos
+    {
+        ((float)windowDimensions[0] / 2.0f) - 6.0f * (0.5f * (float)knight.width / 6.0f),
+        ((float)windowDimensions[1] / 2.0f) - 6.0f * (0.5f * (float)knight.height)
+    };
+
     Vector2 worldMapPos{0.0, 0.0};
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -20,7 +27,7 @@ int main()
 
         Vector2 direction{};
 
-        //movement
+        // moving map opposite of key press to simulate character movement
         if (IsKeyDown(KEY_A)) direction.x -= 1.0;
         if (IsKeyDown(KEY_D)) direction.x += 1.0;
         if (IsKeyDown(KEY_W)) direction.y -= 1.0;
@@ -32,8 +39,13 @@ int main()
             worldMapPos = Vector2Subtract(worldMapPos, Vector2Scale(Vector2Normalize(direction), speed));
         }
         
+        //Drawing the world map
         DrawTextureEx(worldMap, worldMapPos, 0, 6, WHITE);
 
+        //Drawing knight
+        Rectangle knightSourceRect{0.f, 0.f, (float)knight.width / 6.f, (float)knight.height};                                  //knight source rectangle
+        Rectangle knightDestPos{knightPos.x, knightPos.y, (6.f * (float)knight.width / 6.f), (6.f * (float)knight.height)};     //knight destination is middle of screen
+        DrawTexturePro(knight, knightSourceRect, knightDestPos, Vector2{}, 0, WHITE);
 
         EndDrawing();
     }
