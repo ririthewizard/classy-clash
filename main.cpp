5decd52d486bae3260a9bf054c103cfc5b33f659
Riley <ririwilliams.rw@gmail.com>
2024-06-20 23:30:28 -0500

    Created a tick method for the Character class that encapsulates our movement and animating/flipping our sprite

--- main.cpp
+++ main.cpp
@@ -4,12 +4,14 @@
 class Character
 {
 public:
-    Vector2 get_world_pos() { return mWorldPos; }
+    Vector2 getWorldPos() { return mWorldPos; }
+    void setScreenPos(int winWidth, int winHeight);
+    void tick(float deltaTime);
 
 private:
-    Texture2D mTexture;
-    Texture2D mIdle;
-    Texture2D mRun;
+    Texture2D mTexture{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D mIdle{LoadTexture("characters/knight_idle_spritesheet.png")};
+    Texture2D mRun{LoadTexture("characters/knight_run_spritesheet.png")};
     Vector2 mScreenPos;
     Vector2 mWorldPos;
     // 1 : facing right, -1 : facing left
@@ -19,6 +21,51 @@ private:
     int mFrame{};
     const int mMaxFrames{6};
     const float mUpdateTime{1.f / 12.f};
+    const float speed{6.f};
+};
+
+void Character::setScreenPos(int winWidth, int winHeight)
+{
+    mScreenPos = {
+        ((float)winWidth / 2.0f) - 6.0f * (0.5f * (float)mTexture.width / 6.0f),
+        ((float)winHeight / 2.0f) - 6.0f * (0.5f * (float)mTexture.height)};
+};
+
+void Character::tick(float deltaTime)
+{
+    Vector2 direction{};
+
+    // moving map opposite of key press to simulate character movement
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1.0;
+
+    if (Vector2Length(direction) != 0.0)
+    {
+        // set mWorldPos = mWorldPos + direction
+        mWorldPos = Vector2Add(mWorldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        // ternary for setting our rightLeft value based on which direction we are moving(i.e facing)
+        direction.x < 0.f ? mRightLeft = -1.f : mRightLeft = 1.f;
+        mTexture = mRun;
+    }
+    else
+    {
+        mTexture = mIdle;
+    }
+
+    mRunningTime += deltaTime;
+    if (mRunningTime >= mUpdateTime)
+    {
+        mFrame++;
+        mRunningTime = 0.f;
+        if (mFrame >= mMaxFrames)
+            mFrame = 0;
+    }
 };
 
 int main()
@@ -100,8 +147,8 @@ int main()
 
         // Drawing knight
         Rectangle knight_source_rect{frame * single_knight_width, 0.f, rightLeft * single_knight_width, single_knight_height}; // knight source rectangle
-        Rectangle knight_dest_pos{knightPos.x, knightPos.y, (6.f * single_knight_width), (6.f * single_knight_height)};      // knight destination is middle of screen
-        DrawTexturePro(knight, knight_source_rect, knight_dest_pos, Vector2{}, 0, WHITE);                                  // drawing knight to the middle of the screen with origin in top left corner (0,0)
+        Rectangle knight_dest_pos{knightPos.x, knightPos.y, (6.f * single_knight_width), (6.f * single_knight_height)};        // knight destination is middle of screen
+        DrawTexturePro(knight, knight_source_rect, knight_dest_pos, Vector2{}, 0, WHITE);                                      // drawing knight to the middle of the screen with origin in top left corner (0,0)
 
         EndDrawing();
     }
